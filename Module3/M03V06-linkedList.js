289ac6b775192a8f192839affb9346d656dba511
Abdur Razzak <sahadathosain420@gmail.comgit config --global user.email sahadathosain420@gmail.com>
2025-11-24 00:20:25 +0600

    create linked list manually

---
+++ Module3/M03V06-linkedList.js
@@ -0,0 +1,19 @@
+class Node{
+    constructor(value){
+        this.data= value;
+        this.next = null;
+    }
+}
+
+const head = new Node(10);
+head.next = new Node(20);
+head.next.next = new Node(30);
+
+console.log(head)
+
+let temp = head;
+
+while(temp != null){
+    console.log(temp.data , "");
+    temp= temp.next;
+}
